4bb406e2a8a370ecf3bb04551e1aa34c461a74e1
Oğuzhan Çoruk <oguzhancoruk@192.168.1.118>
2024-02-27 17:29:10 +0300

    sun hali

--- index.js
+++ index.js
@@ -1,85 +1,81 @@
-  const express = require('express');
-  const http = require('http');
-  const WebSocket = require('ws');
-  const readXlsxFile = require('read-excel-file/node');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const readXlsxFile = require('read-excel-file/node');
 
-  const app = express();
-  const server = http.createServer(app);
-  const wss = new WebSocket.Server({ server });
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-  async function getSpecificRow() {
-    const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleString('tr-TR', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    });
-    const path = './İstanbul.xlsx'; // Specify the file's directory
-    const saat = currentDate.getHours().toString().padStart(2, '0'); // Saati alırken 2 basamaklı olacak şekilde düzenler
-    const dakika = currentDate.getMinutes().toString().padStart(2, '0'); // Dakikayı alırken 2 basamaklı olacak şekilde düzenler
-    const tarihVeSaat = `${formattedDate} ${saat}:${dakika}`; // Türkiye'nin yerel tarihini ve saatini içeren bir dizgi oluşturuyoruz
-    try {
-      const rows = await readXlsxFile(path);
-      const specificRows = rows.filter(row => row[0].includes(formattedDate));
-      const remainingRows = specificRows.map(row => row.slice(2));
-  
-      const allDayDictionaries = remainingRows.map(row => ({
-        "İmsak": row[0],
-        "Güneş": row[1],
-        "Öğle": row[2],
-        "İkindi": row[3],
-        "Akşam": row[4],
-        "Yatsı": row[5]
+async function getSpecificRow() {
+  const currentDate = new Date();
+  // Tarih ve saat bilgisini İstanbul'a göre formatlıyoruz
+  const istanbulDate = new Date(currentDate.toLocaleString("en-US", {timeZone: "Europe/Istanbul"}));
+  const formattedDate = `${istanbulDate.getDate()} ${getMonthName(istanbulDate.getMonth())} ${istanbulDate.getFullYear()}`;
+  const saat = istanbulDate.getHours().toString().padStart(2, '0');
+  const dakika = istanbulDate.getMinutes().toString().padStart(2, '0');
+  const sonuc = `${saat}:${dakika}`;
+  try {
+    const rows = await readXlsxFile('./İstanbul.xlsx');
+    const specificRows = rows.filter(row => row[0].includes(formattedDate));
+    const remainingRows = specificRows.map(row => row.slice(2));
+
+    const allDayDictionaries = remainingRows.map(row => ({
+      "İmsak": row[0],
+      "Güneş": row[1],
+      "Öğle": row[2],
+      "İkindi": row[3],
+      "Akşam": row[4],
+      "Yatsı": row[5]
+    }));
+
+    const result = allDayDictionaries.map(item => {
+      return Object.keys(item).map(vakit => ({
+        saat: item[vakit],
+        vakit: vakit
       }));
-  
-      const result = allDayDictionaries.map(item => {
-        return Object.keys(item).map(vakit => ({
-          saat: item[vakit],
-          vakit: vakit
-        }));
-      }).flat();
-  
-      return { sunuc: tarihVeSaat, data: result }; // Burada sunuc keyini ekledik
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    }).flat();
+
+    return { sunuc: sonuc, data: result }; 
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-  
-  
+}
 
-  function getMonthName(monthIndex) {
-    const months = [
-      "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
-      "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
-    ];
-    return months[monthIndex];
-  }
+// Ayları getiren fonksiyon
+function getMonthName(monthIndex) {
+  const months = [
+    "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+    "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
+  ];
+  return months[monthIndex];
+}
 
-  wss.on('connection', (ws) => {
-    console.log('Yeni bir bağlantı kuruldu.');
+wss.on('connection', (ws) => {
+  console.log('Yeni bir bağlantı kuruldu.');
 
-    ws.on('message', (message) => {
-      console.log(`Alınan mesaj: ${message}`);
-    });
+  ws.on('message', (message) => {
+    console.log(`Alınan mesaj: ${message}`);
+  });
 
-    const interval = setInterval(async () => {
-      if (ws.readyState === WebSocket.OPEN) {
-        try {
-          const data = await getSpecificRow();
-          ws.send(JSON.stringify({ success: true, result: data }));
-        } catch (error) {
-          console.error("Veri alınamadı:", error);
-        }
+  const interval = setInterval(async () => {
+    if (ws.readyState === WebSocket.OPEN) {
+      try {
+        const data = await getSpecificRow();
+        ws.send(JSON.stringify({ success: true, result: data }));
+      } catch (error) {
+        console.error("Veri alınamadı:", error);
       }
-    }, 1500);
+    }
+  }, 1500);
 
-    ws.on('close', () => {
-      console.log('Bağlantı kesildi.');
-      clearInterval(interval);
-    });
+  ws.on('close', () => {
+    console.log('Bağlantı kesildi.');
+    clearInterval(interval);
   });
+});
 
-  server.listen(9001, () => {
-    console.log('Sunucu 9001 portunda çalışıyor.');
-  });
+server.listen(9002, () => {
+  console.log('Sunucu 9001 portunda çalışıyor.');
+});
